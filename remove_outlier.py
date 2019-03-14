8249e823645d8c56b3106a93e0faba2fb41dedf7
Adriano De Marino <adriano.demarino@gmail.com>
2019-03-14 15:06:14 +0100

    Update remove_outlier.py

--- remove_outlier.py
+++ remove_outlier.py
@@ -3,6 +3,30 @@ import numpy as np
 import scipy.stats as stat
 from scipy.stats import iqr as IQR
 
+'''
+z-score based method
+This method will test if the numbers falls outside the three standard deviations. 
+Based on this rule, if the value is outlier, the method will return true, if not, return false.
+'''
+
+def sd_outlier(x, axis = None, bar = 3, side = 'both'):
+    assert side in ['gt', 'lt', 'both'], 'Side should be `gt`, `lt` or `both`.'
+
+    d_z = stat.zscore(x, axis = axis)
+
+    if side == 'gt':
+        return d_z > bar
+    elif side == 'lt':
+        return d_z < -bar
+    elif side == 'both':
+        return np.abs(d_z) > bar
+
+'''
+IQR based method
+This method will test if the value is less than q1 - 1.5 * iqr or 
+greater than q3 + 1.5 * iqr.
+''' 
+    
 def Q3(x, axis = None):
     return np.percentile(x, 25, axis = axis)
 
@@ -40,5 +64,6 @@ def IQR_outlier(x, axis = None, bar = 1.5, side = 'both'):
         return np.logical_or(upper_outlier, lower_outlier)
 
 #test
+#Finally, if you want to filter out the outliers, use a numpy selector.
 x = [2,3,1,4,2,3,4,5,2,3,3,3,3,4,3,2,50,60] #data
 IQR_outlier(np.asarray(x), axis = 0, bar = 1.5, side = 'both') #call function using numpy array
